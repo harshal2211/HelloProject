db1e55b682dbe6fe37844d050fb3cd165027bbe8
harshal2211 <joshiharshal04@gmail.com>
2015-12-08 23:32:19 +0530

    first commit

---
+++ Employee.java
@@ -0,0 +1,11 @@
+package hello
+
+public class Employee{
+	private long empId;
+	private String empName;
+
+	public Employee(long empId, String empName){
+		this.empId = empId;
+		this.empName = empName;
+	}
+}
---
+++ EmployeeTest.java
@@ -0,0 +1,11 @@
+package Hello
+
+public class EmployeeTest{
+
+	public static void main(String[] args){
+		
+		System.out.println("Enter Employee Id");
+		System.out.println("Enter Employee Name");
+		System.out.println();
+	}
+}
